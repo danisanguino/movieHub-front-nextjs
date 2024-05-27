1de0300f7e775d4f968f1b4035969fa5ece68936
Daniel Sanguino <153718537+danisanguino@users.noreply.github.com>
2024-05-27 19:22:38 +0200

    fix(delete form) quit delete form from page to deploy

--- src/app/(pages)/deletemovie/page.tsx
+++ src/app/(pages)/deletemovie/page.tsx
@@ -5,7 +5,7 @@ import DeleteForm from "@/components/form/form-delete";
 
 export default function DeleteMovie() {
     return (
-       <DeleteForm />
-    // <h1>Borrar pelis aqui</h1>
+    //    <DeleteForm />
+    <h1>Borrar pelis aqui</h1>
     )
 }
