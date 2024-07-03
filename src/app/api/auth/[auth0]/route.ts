68855743135969764dd88238b754ce58cae3b2bd
Daniel Sanguino <153718537+danisanguino@users.noreply.github.com>
2024-07-03 19:31:48 +0200

    fixing and change auth0 to deploy add Image nextjs

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -6,8 +6,8 @@ export const GET = handleAuth(
     login: handleLogin ({
         // authorizationParams: {
         //     //Poner el link del back HERE!!!
-        //     audience: "https://moviehubprojectbackend-develop.up.railway.app"
-        //     // audience: "http://localhost:3800"
+        //     // audience: "https://moviehubprojectbackend-develop.up.railway.app"
+        //     audience: "http://localhost:3800"
         // },
         returnTo:"/movies"
     })
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { getSession } from "@auth0/nextjs-auth0";
 import { FaUpload } from "react-icons/fa";
 import { RiLogoutBoxLine } from "react-icons/ri"
+import Image from "next/image";
 
 
 
@@ -14,11 +15,17 @@ export default async function Header() {
     return (
         <>
             <main className="header-container">
-                <Link href="/movies"><img src="./movie-hub-logo_vx2bju.png" alt="logo" className="logo"/></Link>
+                <Link href="/movies">
+                    <Image src="https://res.cloudinary.com/dqm1upnhh/image/upload/v1715183824/movie-hub-logo_vx2bju.png"
+                        height={60}
+                        width={142}
+                        alt="logo"
+                        />
+                </Link>
                 <div className="user">
                     <Link href="/form"><FaUpload className="upload-buton" size={25} /></Link>
                     <h3>Hola {session?.user.name}!</h3>
-                    <Link href="api/auth/logout"><RiLogoutBoxLine className="upload-buton" size={35} /></Link>
+                    <Link href="/api/auth/logout"><RiLogoutBoxLine className="upload-buton" size={35} /></Link>
                     <img src={session?.user.picture} className="img-profile" alt={session?.user.name}/>
                 </div>
             </main>
