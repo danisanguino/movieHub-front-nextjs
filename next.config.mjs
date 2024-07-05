efcf26591eb91e384a41e1145cce514535e07540
Daniel Sanguino <153718537+danisanguino@users.noreply.github.com>
2024-07-05 13:22:10 +0200

    fixed dynamic routes deleting one next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com"
+            },
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
 
